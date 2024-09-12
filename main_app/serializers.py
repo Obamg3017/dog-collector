a6c0d0552c0de6eec279bea888f58838d573271f
FemiBam <Femibam1@gmail.com>
2024-09-12 18:26:27 -0400

    django rep complete

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Dog, Feeding
+from .models import Dog, Feeding, Toy
 
 class DogSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +10,13 @@ class DogSerializer(serializers.ModelSerializer):
      return obj.fed_for_today()
 
 class FeedingSerializer(serializers.ModelSerializer):
-  class Meta:
+   class Meta:
     model = Feeding
     fields = '__all__'
     read_only_fields = ('dog',)
+
+
+class ToySerializer(serializers.ModelSerializer):
+   class Meta:
+        model = Toy
+        fields = '__all__'    
